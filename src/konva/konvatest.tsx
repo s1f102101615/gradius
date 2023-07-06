d839c9ee631e84e48906897d59a834c5d818d8d0
rarandeyo <s1F102301392@iniad.org>
2023-07-07 06:05:58 +0900

    配列

--- src/konva/konvatest.tsx
+++ src/konva/konvatest.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import { Layer, Rect, Stage } from 'react-konva';
 
 function App() {
+  //NOWposinonが現在の変数Setnowposionが変更する式
+  const [nowPosision, setNowPosision] = useState([0, 0]);
+
   return (
-    <Stage width={3000} height={3000}>
+    <Stage width={1920} height={1080}>
       <Layer>
-        <Rect fill="black" width={1920} height={1080} />
+        <Rect fill="white" width={1920} height={1080} />
+        <Rect
+          fill="red"
+          x={60 + 120 * nowPosision[0]}
+          y={60 + 120 * nowPosision[0]}
+          width={120}
+          height={120}
+        />
       </Layer>
     </Stage>
   );
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,8 @@
 import type { TaskModel } from '$/commonTypesWithClient/models';
 import { useAtom } from 'jotai';
-import type { ChangeEvent } from 'react';
 import { useEffect, useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
+import App from 'src/konva/konvatest';
 import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { apiClient } from 'src/utils/apiClient';
 import { returnNull } from 'src/utils/returnNull';
@@ -11,11 +11,9 @@ import { userAtom } from '../atoms/user';
 const Home = () => {
   const [user] = useAtom(userAtom);
   const [tasks, setTasks] = useState<TaskModel[]>();
-  const [label, setLabel] = useState('');
-  const [nowkey, setNowkey] = useState([0, 0]);
-  const inputLabel = (e: ChangeEvent<HTMLInputElement>) => {
-    setLabel(e.target.value);
-  };
+
+  const [nowkey, setNow] = useState([0, 0]);
+
   const fetchTasks = async () => {
     const tasks = await apiClient.tasks.$get().catch(returnNull);
 
@@ -27,14 +25,6 @@ const Home = () => {
   //   });
   //   setNowkey(Number(aa));
   // };
-  const toggleDone = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).patch({ body: { done: !task.done } });
-    await fetchTasks();
-  };
-  const deleteTask = async (task: TaskModel) => {
-    await apiClient.tasks._taskId(task.id).delete();
-    await fetchTasks();
-  };
 
   useEffect(() => {
     fetchTasks();
@@ -43,7 +33,7 @@ const Home = () => {
   const createNum = async () => {
     const a = await apiClient.control.post({ body: { x: 12, y: 11, a: 1 } });
     console.log(a.body.x, a.body.y);
-    setNowkey([Number(a.body.x), Number(a.body.y)]);
+    setNow([Number(a.body.x), Number(a.body.y)]);
   };
   if (!tasks || !user) return <Loading visible />;
 
@@ -55,7 +45,7 @@ const Home = () => {
       <p>{nowkey}</p>
       <button onClick={createNum} />
 
-      {/* <App /> */}
+      <App />
     </>
   );
 };
