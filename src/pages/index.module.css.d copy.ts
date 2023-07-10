5b044df77d5079dd36da1f00319de7bdec0f483b
s1f102101615 <s1f102101615@iniad.org>
2023-07-10 20:05:36 +0900

    controllerURL追加

---
+++ src/pages/index.module.css.d copy.ts
@@ -0,0 +1,8 @@
+declare const styles:
+  & Readonly<{ "title": string }>
+  & Readonly<{ "tasks": string }>
+  & Readonly<{ "tasks": string }>
+  & Readonly<{ "deleteBtn": string }>
+;
+export default styles;
+//# sourceMappingURL=./index.module.css.d.ts.map
