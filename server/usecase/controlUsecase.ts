9635c6702b35f30476656a96d8207ddb8bc443ad
s1f102101615 <s1f102101615@iniad.org>
2023-07-12 15:37:29 +0900

    gradius風の画面サイズにした

--- server/usecase/controlUsecase.ts
+++ server/usecase/controlUsecase.ts
@@ -6,29 +6,29 @@
 export const controlUsecase = {
   key: (x: number, y: number, KeyEvent: string) => {
     if (KeyEvent === 'ArrowUp') {
-      if (x === 0) {
+      if (x <= 0) {
         return { x, y };
       }
       console.log('ArrowUp');
-      const newX = x - 2;
+      const newX = x - 7;
       return { x: newX, y };
     }
     if (KeyEvent === 'ArrowDown') {
       console.log('ArrowDown');
-      const newX = x + 2;
+      const newX = x + 7;
       return { x: newX, y };
     }
     if (KeyEvent === 'ArrowLeft') {
-      if (y === 0) {
+      if (y <= 0) {
         return { x, y };
       }
       console.log('ArrowLeft');
-      const newY = y - 2;
+      const newY = y - 7;
       return { x, y: newY };
     }
     if (KeyEvent === 'ArrowRight') {
       console.log('ArrowRight');
-      const newY = y + 2;
+      const newY = y + 7;
       return { x, y: newY };
     }
     return { x, y };
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       const bullet = { x: nowkey[1] + 54, y: nowkey[0] + 20, speedX: 1000 };
       setGradius_bullet((prevGradius_bullet) => [...prevGradius_bullet, bullet]);
     } else if (e.code === 'Space') {
-      const enemyspwan = { x: 1280, y: Math.floor(Math.random() * 721), speedX: -100 };
+      const enemyspwan = { x: 640, y: Math.floor(Math.random() * 481), speedX: -100 };
       setEnemy((prevEnemy) => [...prevEnemy, enemyspwan]);
     }
     const a = await apiClient.control.post({
@@ -75,7 +75,7 @@ const Home = () => {
                 ...bullet,
                 x: bullet.x + bullet.speedX * timeDiff,
               }))
-              .filter((bullet) => bullet.x < 1290) // 画面の右端に到達していない弾のみをフィルタリング
+              .filter((bullet) => bullet.x < 640) // 画面の右端に到達していない弾のみをフィルタリング
         );
         // 敵の動き
         setEnemy((prev) =>
@@ -114,7 +114,7 @@ const Home = () => {
         onKeyDown={keyDownHandler}
         style={{ display: 'inline-block', border: 'solid' }}
       >
-        <Stage width={1280} height={720}>
+        <Stage width={640} height={480}>
           <Layer>
             <Rect x={nowkey[1]} y={nowkey[0]} width={50} height={40} fill="blue" />
             {enemy.map((state, index) => (
